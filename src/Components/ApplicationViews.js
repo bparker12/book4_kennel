636428b162c5c9a0b8fe564562e4e46c7619ad71
bparker12 <49100894+bparker12@users.noreply.github.com>
2019-07-15 12:00:42 -0500

    add API to pull data on each subject

--- src/Components/ApplicationViews.js
+++ src/Components/ApplicationViews.js
@@ -6,39 +6,12 @@ import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
 
 class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animalsFromAPI = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    ownersFromAPI = [
-        { id: 1, phoneNumber: "555-555-1234", name:"Bob Saget" },
-        { id: 2, phoneNumber: "555-555-2345", name:"Vince Vaughn" },
-        { id: 3, phoneNumber: "555-555-3456", name:"Ben Stiller" },
-        { id: 4, phoneNumber: "555-555-4567", name:"Dwayne Johnson" }
-    ]
 
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animalsFromAPI,
-        owners: this.ownersFromAPI,
+        employees: [],
+        locations: [],
+        animals: [],
+        owners: [],
     }
 
     render() {
